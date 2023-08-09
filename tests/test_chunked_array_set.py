32daf804eb4f6c4558408d7f443c0c7c3c3e3b9e
Jose Blanca <jblanca@upv.es>
2023-08-09 10:52:34 +0200

    test fixed: it wasn't comparing the arrays

--- tests/test_chunked_array_set.py
+++ tests/test_chunked_array_set.py
@@ -1,8 +1,13 @@
 import tempfile
+import functools
 
 from chunked_array_set import ChunkedArraySet
 
-from .test_utils import generate_chunks, check_chunks_are_equal
+from .test_utils import (
+    generate_chunks,
+    check_chunks_are_equal,
+    check_arrays_in_two_dicts_are_equal,
+)
 
 
 def test_write_chunked_array_set():
@@ -18,3 +23,37 @@ def test_write_chunked_array_set():
         chunks.extend(new_chunks)
         mem_array_set.extend_chunks(new_chunks)
         disk_array_set.extend_chunks(new_chunks)
+
+
+def test_pipeline():
+    chunks = generate_chunks(num_chunks=2)
+    array_set = ChunkedArraySet(chunks=chunks)
+
+    map_functs = [lambda chunk: {id: array * 2 for id, array in chunk.items()}]
+
+    def sum_columns(current_sum, chunk):
+        for id in chunk.keys():
+            array = chunk[id]
+            this_sum = array.sum(axis=0)
+            if current_sum is None:
+                current_sum = {}
+            accumulated_sum_for_this_array = current_sum.get(id)
+            if accumulated_sum_for_this_array is None:
+                current_sum[id] = this_sum
+            else:
+                current_sum[id] = current_sum[id] + this_sum
+        return current_sum
+
+    result_chunks = map(map_functs[0], chunks)
+    expected_result = functools.reduce(sum_columns, result_chunks, None)
+    result = array_set.run_pipeline(
+        map_functs=map_functs, reduce_funct=sum_columns, reduce_initialializer=None
+    )
+
+    check_arrays_in_two_dicts_are_equal(expected_result, result)
+
+    result_chunks = list(map(map_functs[0], chunks))
+    processed_chunks = array_set.run_pipeline(map_functs=map_functs)
+    processed_array_set = ChunkedArraySet(chunks=processed_chunks)
+
+    check_chunks_are_equal(processed_array_set.chunks, result_chunks)
